07bb8e883878794763a1ffc2f0ab00f34bca9dcc
Gabriel <gb_souza@id.uff.br>
2023-11-13 15:52:14 -0300

    add graphs to dashboard

--- bugtracker/src/app/app.module.ts
+++ bugtracker/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -25,7 +25,8 @@ import { TicketComponent } from './ticket/ticket.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- bugtracker/src/app/dashboard/dashboard.component.ts
+++ bugtracker/src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+// dashboard.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  pieChartData: any[] = [{ data: [30, 30, 30], backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'], label: 'Tickets Status' }];
+  pieChartLabels: string[] = ['Abertos', 'Em Andamento', 'Concluídos'];
+
+  pieChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  pieChartLegend = true;
+  pieChartPlugins = [];
+
+  // New data for the second chart (you can customize it)
+  barChartData: any[] = [{ data: [30, 30, 30], backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'], label: 'Tickets Status' }];
+  barChartLabels: string[] = ['Categoria 1', 'Categoria 2', 'Categoria 3', 'Categoria 4'];
+
+  barChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  barChartLegend = true;
+  barChartPlugins = [];
+
+
+  // New data and options for the line chart (Projects)
+  lineChartData: any[] = [
+    { data: [10, 20, 15, 25, 30], label: 'Bugs durante a semana' }
+  ];
+  lineChartLabels: string[] = ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4', 'Semana 5'];
+
+  lineChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+    // Add any additional options for the line chart here
+  };
+
+  lineChartLegend = true;
+  lineChartPlugins = [];
 
 }
